6c3d0bfa6cf898d9651a9b48ad8a44a9cc3874c7
Vladyslav_Bashtannyk <70664311+flyosprey@users.noreply.github.com>
2021-08-24 14:12:59 +0300

    Update code. Get airtable data

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,22 @@
+import requests
+from operator import itemgetter
 import json
 
-def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello, AWC! Done')
-    }
+
+def lambda_function(event, context):
+    api_key = "keylMkRVCV25NxOBl"
+    url = "https://api.airtable.com/v0/appW1Vv1p4IHxtiWQ/tblX1jGmYZoqyHrlE"
+    headers = {"Authorization": "Bearer " + api_key}
+
+    response = requests.get(url, headers=headers)
+    airtable_response = response.json()
+    airtable_records = (airtable_response["records"])
+
+    for i in range(len(airtable_records)):
+        airtable_records[i]["id"] = airtable_records[i]["fields"]["ID"]
+
+    sorted_airtable_records = sorted(airtable_records, key=itemgetter('id'))
+
+    titles = [sorted_airtable_records[i]["fields"]["title"] for i in range(len(sorted_airtable_records))]
+    
+    return json.dumps(titles)
