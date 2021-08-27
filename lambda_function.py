3f7a444f429e9b4374892788a88ba18a73a0ee4c
Vladyslav_Bashtannyk <70664311+flyosprey@users.noreply.github.com>
2021-08-27 16:13:58 +0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,5 +5,5 @@ from my_classes.buffer_output import BufferOutput
 if __name__ == "__main__":
     air = AirTableApi()
     titles = air.get_all_titles()
-    buffer = BufferOutput(titles)
-    buffer.buffer_manage()
+    buf = BufferOutput(titles)
+    buf.buffer_manage()
